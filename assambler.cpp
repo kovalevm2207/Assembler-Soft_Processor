3d9b86cd9f5050eaf6075259b53f8d74c21d3f51
kovalevm2207 <kovalevm220741@gmail.com>
2025-10-03 04:42:24 +0300

    Начало положено, написан не работающий прототип ассемблера

---
+++ assambler.cpp
@@ -0,0 +1,144 @@
+#include <stdio.h>
+#include <cstring>
+#include <math.h>
+#include <assert.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <fcntl.h>
+
+#include "my_stack.h"
+#include "commands.h"
+
+struct retranslator_s {
+    int count;
+    unsigned char* buffer;
+    command_t command;
+    FILE* file_name;
+};
+
+command_t find_command(retranslator_s* retranslator);
+void do_command(retranslator_s* retranslator);
+void do_help(void);
+void do_push(retranslator_s* retranslator);
+void do_pow(retranslator_s* retranslator);
+
+int main()
+{
+    FILE* file = fopen("source.asm", "r");
+    assert(file != 0);
+
+    unsigned char buffer[10000] = {};
+    retranslator_s retranslator = {
+        .count = 0,
+        .buffer = buffer,
+        .command = START_PROGRAM,
+        .file_name = file
+    };
+
+    while (retranslator.command != END_PROGRAM) {
+        retranslator.command = find_command(&retranslator);
+        do_command(&retranslator);
+    };
+
+    printf("Result codes: ");
+    for (int i = 0; i < retranslator.count; i++) {
+        printf("%d ", buffer[i]);
+    }
+    printf("\n");
+
+    fclose(file);
+
+    return 0;
+}
+
+
+command_t find_command(retranslator_s* retranslator)
+{
+    char command[20] = {};
+    fscanf(retranslator->file_name, "%s", command);
+    if (strcmp(command,  "HELP") == 0) return HELP;
+    if (strcmp(command,  "PUSH") == 0) return PUSH;
+    if (strcmp(command,   "ADD") == 0) return ADD;
+    if (strcmp(command,   "SUB") == 0) return SUB;
+    if (strcmp(command,   "DIV") == 0) return DIV;
+    if (strcmp(command,   "OUT") == 0) return OUT;
+    if (strcmp(command,   "MUL") == 0) return MUL;
+    if (strcmp(command,   "POW") == 0) return POW;
+    if (strcmp(command,  "SQRT") == 0) return SQRT;
+    if (strcmp(command,   "HLT") == 0) return END_PROGRAM;
+    if (strcmp(command, "RESET") == 0) return RESET_STK;
+    else return INVALID_COMMAND;
+}
+
+
+void do_command(retranslator_s* retranslator)
+{
+    assert(retranslator != NULL);
+
+    unsigned char* buffer = retranslator->buffer;
+    int count = retranslator->count;
+    command_t command = retranslator->command;
+
+    if (command == END_PROGRAM) printf("You end the calc program\n");
+    if (command == HELP)      do_help();
+    if (command == PUSH)      do_push(retranslator);
+    if (command == ADD)       buffer[count++] = 2;
+    if (command == SUB)       buffer[count++] = 3;
+    if (command == DIV)       buffer[count++] = 4;
+    if (command == OUT)       buffer[count++] = 5;
+    if (command == MUL)       buffer[count++] = 6;
+    if (command == POW)       do_pow(retranslator);
+    if (command == SQRT)      buffer[count++] = 8;
+    if (command == RESET_STK) buffer[count++] = 9;
+    if (command == INVALID_COMMAND) {
+        printf(CHANGE_ON RED TEXT_COLOR "INVALID_COMMAND, "
+               CHANGE_ON RED TEXT_COLOR "please read help:\n" RESET);
+        do_help();
+    }
+
+    retranslator->count = count;
+}
+
+
+void do_push(retranslator_s* retranslator)
+{
+    assert(retranslator != NULL);
+
+    retranslator->buffer[retranslator->count++] = 1;
+    int data = 0;
+    fscanf(retranslator->file_name, "%d", &data);
+    retranslator->buffer[retranslator->count++] = data;
+}
+
+
+void do_pow(retranslator_s* retranslator)
+{
+    assert(retranslator != NULL);
+
+    retranslator->buffer[retranslator->count++] = 7;
+    int n = 0;
+    fscanf(retranslator->file_name, "%d", &n);
+    retranslator->buffer[retranslator->count++] = n;
+}
+
+
+void do_help(void)
+{
+    printf("\nusage: main_calc " CHANGE_ON BLUE TEXT_COLOR "[HELP] [HLT] [PUSH] [OUT]\n"
+           "                 [SUB] [DIV] [MUL] [POW] [SQRT]\n"
+           RESET "\n"
+	       "These are common main_calc commands used in various situations:\n"
+           "You can use " CHANGE_ON PURPLE TEXT_COLOR "only integers" RESET " value\n"
+	       "\n"
+           CHANGE_ON GREEN TEXT_COLOR "HELP     " RESET "         cli flags help message\n"
+	       CHANGE_ON GREEN TEXT_COLOR "HLT      " RESET "         HaLT (end the program)\n"
+           CHANGE_ON GREEN TEXT_COLOR "PUSH  num" RESET "         save the value (num) to calculator memory\n"
+           CHANGE_ON GREEN TEXT_COLOR "OUT      " RESET "         retrieve the last saved value from calculator memory\n"
+           CHANGE_ON GREEN TEXT_COLOR "ADD      " RESET "         add the two last numbers stored in memory\n"
+           CHANGE_ON GREEN TEXT_COLOR "SUB      " RESET "         subtract the last number in memory from the second last number in memory\n"
+           CHANGE_ON GREEN TEXT_COLOR "DIV      " RESET "         divide the last number stored in memory by the second last number stored in memory\n"
+           CHANGE_ON GREEN TEXT_COLOR "MUL      " RESET "         multiply the two last numbers stored in memory\n"
+           CHANGE_ON GREEN TEXT_COLOR "POW   num" RESET "         raise the last number stored in memory to a power (num)\n"
+           CHANGE_ON GREEN TEXT_COLOR "SQRT     " RESET "         calculate the square root of the last number in memory\n"
+           "\n");
+}
---
+++ commands.h
@@ -0,0 +1,16 @@
+typedef enum
+{
+    HELP            = 0,
+    PUSH            = 1,
+    ADD             = 2,
+    SUB             = 3,
+    DIV             = 4,
+    OUT             = 5,
+    MUL             = 6,
+    POW             = 7,
+    SQRT            = 8,
+    RESET_STK       = 9,
+    INVALID_COMMAND = 10,
+    START_PROGRAM   = 11,
+    END_PROGRAM     = 12
+} command_t;
